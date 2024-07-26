1ce94645f57b991e787bf5525a44509d420be745
carlosjhoan <carlosjhoanaguilar@gmail.com>
2024-07-26 16:33:39 -0500

    Creación del controlador con las listas de las funcionalidades del programa

--- crudfiltro/src/main/java/com/crudfiltro/ControladorLFucnionalidadesIntro/ControladorFuncionalidadesIntro.java
+++ crudfiltro/src/main/java/com/crudfiltro/ControladorLFucnionalidadesIntro/ControladorFuncionalidadesIntro.java
@@ -1,8 +1,15 @@
 package com.crudfiltro.ControladorLFucnionalidadesIntro;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.crudfiltro.ActualizarInformacionPersona.Adapter.In.IrActualizarInformacionPersona;
+import com.crudfiltro.AsignarHabilidad.Adapter.In.IrAsignarHabilidad;
 import com.crudfiltro.ClasesAbstractas.Funcionalidad;
+import com.crudfiltro.ConsultarPersonaHabilidad.Adpater.In.IrConsultarPersonaHabilidad;
+import com.crudfiltro.CrearHabilidad.Adapter.In.IrCrearHabilidad;
+import com.crudfiltro.EliminarPersona.Adapter.In.IrEliminarPersona;
+import com.crudfiltro.RegistrarPersona.Adapter.In.IrRegistrarPersona;
 
 public class ControladorFuncionalidadesIntro {
 
@@ -10,6 +17,28 @@ public class ControladorFuncionalidadesIntro {
 
     private static ControladorFuncionalidadesIntro CONTROLLER = new ControladorFuncionalidadesIntro();
 
-    
+    private ControladorFuncionalidadesIntro() {
+
+
+        // Inicialización de la lista para el menú inicial
+        lstIntro = new ArrayList<>();
+
+        // Instanciar la funcionalidades para el menú principal
+        Funcionalidad registrarPersona = new IrRegistrarPersona("Registrar persona");
+        Funcionalidad crearHabilidad = new IrCrearHabilidad("Crear habilidad");
+        Funcionalidad actualizaInfoPersona = new IrActualizarInformacionPersona("Actualizar persona");
+        Funcionalidad asignarHabilidad = new IrAsignarHabilidad("Asignar habilidad");
+        Funcionalidad consultarPersonaHabilidad = new IrConsultarPersonaHabilidad("Consultar persona por habilidad");
+        Funcionalidad eliminarPersona = new IrEliminarPersona("Eliminar Persona");
+
+        // Añadir funcionalidades a la lstIntro
+        lstIntro.add(registrarPersona);
+        lstIntro.add(crearHabilidad);
+        lstIntro.add(actualizaInfoPersona);
+        lstIntro.add(asignarHabilidad);
+        lstIntro.add(consultarPersonaHabilidad);
+        lstIntro.add(eliminarPersona);
+
+    }
 
 }
